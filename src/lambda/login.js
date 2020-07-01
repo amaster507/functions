0691266c21f75152cc72e5a3eb89b604f485ed40
amaster507 <themaster5.07@gmail.com>
2020-07-01 10:25:19 -0500

    changes to login function

--- src/lambda/login.js
+++ src/lambda/login.js
@@ -4,6 +4,9 @@ import { v4 as uuid } from 'uuid';
 import querystring from "querystring"
 import fetch from "node-fetch"
 
+// FIXME: Change this key and pull it from env variable
+const key = "Fmg>MN/+0*^-V'8j<mr!GWq1knBXai"
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -17,19 +20,29 @@ exports.handler = async (event, context) => {
   // const fromIP = event.headers?.["x-nf-client-connection-ip"] || "N/A"
   const AUTH_TOKEN = jwt.sign({
     "https://missionbase.com/jwt/claims": {
+      "PASSWORD": pass,
       "USERNAME": username,
-      "PASS": pass
+      "IS_LOGIN_API": true,
     }
-  }, "secretkey", {
-    expiresIn: "10 seconds",
+  }, key, {
+    expiresIn: "10 minutes",
     issuer: "missionbase login",
     jwtid: uuid(),
     subject: "login"
   })
-  return {
-    statusCode: 200,
-    body: AUTH_TOKEN
-  }
+  return fetch("http://3.231.127.133:8080/graphql", {
+    headers: {
+      auth: AUTH_TOKEN
+    },
+    method: "POST",
+    body: 
+  }).then(() => ({
+      statusCode: 200,
+      body: JSON.stringify({
+        query: `query ($username: String!){ getUser(username:$username) { username isActive userId hasRole hasPassword { password } hasTypes hasGroupAccess { id hasRights { id name isTrue } forGroup { slug name isContact { id organization } isActive } } isContact { id firstName lastName } } }`,
+        variables: { "username": username }
+      })
+    }))
 
 
   // Send greeting to Slack
